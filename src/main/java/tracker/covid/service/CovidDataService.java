84006f617ffa7c32fa8ee2526ba5446dbab1f02e
Senad <dellic.senad@gmail.com>
2020-12-07 16:18:17 +0100

    Refactor

--- src/main/java/tracker/covid/service/CovidDataService.java
+++ src/main/java/tracker/covid/service/CovidDataService.java
@@ -17,12 +17,13 @@ import java.util.List;
 @Getter
 @Service
 public class CovidDataService {
-    List<Covid> covidData = new ArrayList<>();
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"); // MM/DD/YYYY HH:mm:ss
     private static final String VIRUS_DATA_URL =
             "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/12-03-2020.csv";
+    private List<Covid> covidData = new ArrayList<>();
 
     @PostConstruct
-    public void getVirusData() throws IOException {
+    private void getVirusData() throws IOException {
         URL url = new URL(VIRUS_DATA_URL);
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
         BufferedReader input = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
@@ -32,10 +33,9 @@ public class CovidDataService {
     }
 
     private void readCSVFile(CSVReader csvReader) throws IOException {
+        List<Covid> newStats = new ArrayList<>();
         String[] line;
         int i = 0;
-        List<Covid> newStats = new ArrayList<>();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"); // MM/DD/YYYY HH:mm:ss
 
         while ((line = csvReader.readNext()) != null) {
             if (i == 0) {
@@ -50,14 +50,15 @@ public class CovidDataService {
              */
             Covid covid = new Covid();
             try {
-                covid.setLastUpdate(LocalDateTime.parse(line[4], formatter));
+                covid.setLastUpdate(LocalDateTime.parse(line[4], FORMATTER));
                 covid.setConfirmed(Long.valueOf(line[7]));
                 covid.setDeath(Long.valueOf(line[8]));
                 covid.setRecovered(Long.valueOf(line[9]));
                 covid.setActive(Long.valueOf(line[10]));
                 covid.setCombinedKey(line[11]);
-            } catch (NumberFormatException ex) {
-                System.err.println("Invalid string in argument"); //request for well-formatted string
+
+            } catch (NumberFormatException ex) { //request for well-formatted string
+                System.err.println("Invalid string in argument");
             }
             newStats.add(covid);
         }
